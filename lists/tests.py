c6b4c1134e9e3f10a1b6f6d40e4cad353cc1347c
boba <qucals@gmail.com>
2021-10-29 20:49:29 +0500

    The base view returns the minimal HTML content

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -17,3 +18,17 @@ class HomePageTest(TestCase):
 
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_connect_html(self):
+        """
+        Тест: домашняя страница возвращает правильный html
+        """
+
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
