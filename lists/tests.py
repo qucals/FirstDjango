27290d2fe9b2e557f3222de40bba470d9f307c80
boba <qucals@gmail.com>
2021-10-29 20:39:11 +0500

    The first module test and url convertion

---
+++ lists/tests.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import resolve
+
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    """
+    Тест домашней страницы
+    """
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """
+        Тест: корневой url преобразуется в представление
+        домашней страницы
+        """
+
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
