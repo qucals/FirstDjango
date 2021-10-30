5f8a0766cc8f452befdce5d53455423af1c4701f
boba <qucals@gmail.com>
2021-10-30 17:29:12 +0500

    Refactored the home's view for for can be used the template

--- lists/tests.py
+++ lists/tests.py
@@ -24,11 +24,17 @@ class HomePageTest(TestCase):
         Тест: домашняя страница возвращает правильный html
         """
 
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
+        response = self.client.get('/')
 
+        html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
 
+    def test_uses_home_template(self):
+        """
+        Тест: используется домашний шаблон
+        """
+
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
