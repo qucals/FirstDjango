15dcc7b6461fcfd691b01e865a1e936279522963
boba <qucals@gmail.com>
2021-10-30 17:15:01 +0500

    The functional test checks posibility to create a new elemnt of the todo-list

--- functionl_tests.py
+++ functionl_tests.py
@@ -1,6 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -23,19 +26,34 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что загловок и шапка страницы говорят о списках
         # неотложенных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element(By.TAG_NAME, value='h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element(By.ID, value='id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Она набирает в текстовом поле "Купить павлинья перья" (ее хобби -
         # вязание рыболвных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Куаить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_element(by=By.TAG_NAME, value='tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлинье перья' for row in rows)
+        )
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
 
