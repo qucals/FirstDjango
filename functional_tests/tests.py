c4f3068036229c954ca06a3e6072ebe97f39787b
boba <qucals@gmail.com>
2021-11-05 10:05:47 +0500

    New URL + views for adding into an existing list. FT is successfully

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -126,7 +126,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Опять-таки, нет ни следа от списка Эдит
-        page_text = self.browser.find_element(By=By.TAG_NAME, value='body').text
+        page_text = self.browser.find_element(by=By.TAG_NAME, value='body').text
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Купить молоко', page_text)
 
--- lists/tests.py
+++ lists/tests.py
@@ -86,7 +86,8 @@ class ListViewTest(TestCase):
         Тест: используется шаблон списка
         """
 
-        response = self.client.get('/lists/.../')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
@@ -94,15 +95,31 @@ class ListViewTest(TestCase):
         Тест: отображаются все элементы списка
         """
 
-        list_ = List.objects.create()
+        correct_list = List.objects.create()
 
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
 
-        response = self.client.get('/lists/.../')
+        other_list = List.objects.create()
+        Item.objects.create(text='another itemey of the 1th list', list=other_list)
+        Item.objects.create(text='another itemey of the 2nd list', list=other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'another itemey of the 1th list')
+        self.assertNotContains(response, 'another itemey of the 2nd list')
+
+    def test_passes_correct_list_to_template(self):
+        """
+        Тест: передается правильный шаблон списка
+        """
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
 
 
 class NewListTest(TestCase):
@@ -126,4 +143,39 @@ class NewListTest(TestCase):
         """
 
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/.../')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_can_save_a_post_request_to_an_existing_list(self):
+        """
+        Тест: можно сохранить post-запрос в существующий список
+        """
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_link_view(self):
+        """
+        Тест: переадресуется в представление списка
+        """
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
